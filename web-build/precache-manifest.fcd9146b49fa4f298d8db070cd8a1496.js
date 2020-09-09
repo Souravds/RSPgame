ea572d879433704b44708a5767bc82225456d5be
Sourav Das <Souravds@users.noreply.github.com>
2020-09-09 13:02:22 -0400

    added web build

---
+++ web-build/precache-manifest.fcd9146b49fa4f298d8db070cd8a1496.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "e129a3db831fa8d2ca62",
+    "url": "/static/js/app.808516f9.chunk.js"
+  },
+  {
+    "revision": "66090dc4495d4ce3dfb9",
+    "url": "/static/js/runtime~app.2e9f1821.js"
+  },
+  {
+    "revision": "7ca02bf67a2afdc22f32",
+    "url": "/static/js/2.cc2ebccd.chunk.js"
+  },
+  {
+    "revision": "ec5a954f2f97c3fdfcbc31ee6593de42",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "f328c398676e066fb4e76f0c028592b4",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "e802d2644326fd9fa701f1d969a8a94a",
+    "url": "/register-service-worker.js"
+  },
+  {
+    "revision": "a990a8c98c379b1c14de33443329d80a",
+    "url": "/static/js/2.cc2ebccd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4c9d35375eb8cb28721cdd4d423b3e9",
+    "url": "/manifest.json"
+  }
+];
